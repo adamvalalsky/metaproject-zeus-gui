2b7b0d30e4b30dc6561f56ef450faf2dc6a9a84c
Adam Valalský <492900@mail.muni.cz>
2023-11-21 23:03:31 +0100

    feat: create basic docker compose

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      clientPort: 5137
+    },
+    port: 5137,
+    watch: {
+      usePolling: true
+    }
+  }
 })
