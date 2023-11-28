2bb44095375dc79aa95c04ecb57e82f32179cf01
Adam Valalský <492900@mail.muni.cz>
2023-11-28 22:29:46 +0100

    feat: create basic route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Index from "./routes/Index";
 
 function App() {
 	return (
 		<React.StrictMode>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<div>Hello world</div>} />
+					<Route path="/" element={<Index />} />
 					<Route path="/contacts" element={<div>Hello contacts</div>} />
 				</Routes>
 			</BrowserRouter>
