3ff3d121a26e75e833a75ac9bcb931d515e5eba6
Adam Valalský <492900@mail.muni.cz>
2023-11-28 22:24:22 +0100

    fix(web): fixed eslint and prettier errors

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './App.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 function App() {
-    return (
-        <React.StrictMode>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<div>Hello world</div>}></Route>
-                    <Route path='/contacts' element={<div>Hello contacts</div>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </React.StrictMode>
-    )
+	return (
+		<React.StrictMode>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<div>Hello world</div>} />
+					<Route path="/contacts" element={<div>Hello contacts</div>} />
+				</Routes>
+			</BrowserRouter>
+		</React.StrictMode>
+	);
 }
 
-export default App
+export default App;
