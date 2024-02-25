1e211516d765a58a7381451f064169834fb46aea
Adam Valalský <492900@mail.muni.cz>
2024-02-25 20:58:05 +0100

    feat: added Gitlab CI tests

--- web/src/components/PrivateRoute/index.tsx
+++ web/src/components/PrivateRoute/index.tsx
@@ -2,7 +2,8 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../modules/auth/context.tsx';
 
-const PrivateRoute = ({ component: Component, ...children }) => {
+// eslint-disable-next-line
+const PrivateRoute = ({ component: Component, ...children }: any) => {
 	const { isAuthenticated } = useContext(AuthContext);
 
 	if (isAuthenticated()) {
