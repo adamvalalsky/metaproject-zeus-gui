66a10f70e8d946187efb7d8b8fa89a6dcd6585da
Adam Valalský <492900@mail.muni.cz>
2024-03-05 23:46:47 +0100

    feat: hide or show menu depending on screen size

--- web/src/components/PrivateRoute/index.tsx
+++ web/src/components/PrivateRoute/index.tsx
@@ -1,13 +1,19 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../modules/auth/context.tsx';
 import AppMenu from '../AppMenu';
+import useWindowSize from '../../hooks/useWindowSize.ts';
 import { MainContainer } from './styled.tsx';
 
 // eslint-disable-next-line
 const PrivateRoute = ({ component: Component, ...children }: any) => {
 	const { isAuthenticated } = useContext(AuthContext);
-	const [isOpen, setIsOpen] = useState(true);
+	const windowSize = useWindowSize();
+	const [isOpen, setIsOpen] = useState(windowSize > 1000);
+
+	useEffect(() => {
+		setIsOpen(windowSize > 1000);
+	}, [windowSize]);
 
 	if (!isAuthenticated()) {
 		return <Navigate to="/" replace />;
