4d09b08f68dd76f91ce3204f988b1630ec83744a
Adam Valalský <492900@mail.muni.cz>
2024-07-29 20:13:30 +0200

    fix: fixed file download

--- web/src/modules/file/download.ts
+++ web/src/modules/file/download.ts
@@ -1,10 +1,8 @@
-import request from '@/modules/api/request';
-import { type ApiResponse } from '@/modules/api/model';
+import { download } from '@/modules/api/request';
 
 export type FileDownloadType = 'archival';
 
 export const downloadFile = async (projectId: number, processType: FileDownloadType) => {
-	const response = (await request(`/project/${projectId}/file/${processType}`)) as ApiResponse<string>;
-
-	return new Blob([response.data], response.type ? { type: response.type } : { type: 'application/octet-stream' });
+	const response = await download(`/project/${projectId}/file/${processType}`);
+	return response.data;
 };
