1b6ec612f3434a82c0861435cdae2a7c052dcd17
Adam Valalský <492900@mail.muni.cz>
2024-11-26 21:24:55 +0100

    feat: show allocation detail

--- web/src/modules/allocation/model.ts
+++ web/src/modules/allocation/model.ts
@@ -7,3 +7,77 @@ export type Allocation = {
 		type: string;
 	};
 };
+
+export type AllocationDetail = {
+	/**
+	 * Allocation id
+	 */
+	id: number;
+
+	/**
+	 * Allocation status
+	 */
+	status: string;
+
+	/**
+	 * Allocation start date
+	 */
+	startDate: string;
+
+	/**
+	 * Allocation end date
+	 */
+	endDate: string;
+
+	project: {
+		id: number;
+		title: string;
+	};
+
+	/**
+	 * Allocation resource
+	 */
+	resource: {
+		/**
+		 * Allocation resource id
+		 */
+		id: number;
+		/**
+		 * Allocation resource name
+		 */
+		name: string;
+
+		/**
+		 * Allocation resource type
+		 */
+		type: string;
+	};
+
+	/**
+	 * Allocation justification
+	 */
+	justification: string;
+
+	/**
+	 * Allocation description
+	 */
+	description: string;
+
+	/**
+	 * Allocation quantity
+	 */
+	quantity: number | null;
+
+	/**
+	 * Allocation is locked
+	 */
+	isLocked: boolean;
+
+	updatedAt: string;
+
+	allocationUsers: {
+		id: number;
+		name: string;
+		email: string;
+	}[];
+};
--- web/src/routes/project/detail/allocation/detail.tsx
+++ web/src/routes/project/detail/allocation/detail.tsx
@@ -1,14 +1,37 @@
 import { useParams } from 'react-router';
-import { Box, Title } from '@mantine/core';
+import { Badge, Box, Divider, Flex, Group, Stack, Text, Title } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
+import dayjs from 'dayjs';
+import { DataTable } from 'mantine-datatable';
+import React from 'react';
 
 import PageBreadcrumbs from '@/components/global/page-breadcrumbs';
 import { useProjectOutletContext } from '@/modules/auth/guards/project-detail-guard';
+import { useAllocationDetailQuery } from '@/modules/allocation/api/allocation-detail';
+import notFound from '@/components/global/not-found';
+import ErrorPage from '@/components/global/error-page';
+import Loading from '@/components/global/loading';
 
 const AllocationDetail = () => {
 	const { t } = useTranslation();
 	const { project } = useProjectOutletContext();
 	const { allocationId } = useParams();
+
+	if (!allocationId || isNaN(+allocationId)) {
+		return notFound();
+	}
+
+	const { data: allocation, isPending, isError } = useAllocationDetailQuery(+allocationId);
+
+	if (isError) {
+		return <ErrorPage />;
+	}
+
+	if (isPending) {
+		return <Loading />;
+	}
+
+	console.log(allocation);
 
 	return (
 		<Box>
@@ -19,7 +42,80 @@ const AllocationDetail = () => {
 					{ title: 'Request allocation', href: `/project/${project.id}/allocation` }
 				]}
 			/>
-			<Title>{t('routes.AllocationDetail.title')}</Title>
+			<Group justify="space-between">
+				<Title>Allocation detail</Title>
+				<Group>
+					<Badge
+						variant="light"
+						size="lg"
+						color={allocation.status === 'active' ? 'teal' : allocation.status === 'new' ? 'orange' : 'red'}
+					>
+						{t(`routes.ProjectDetail.status.${allocation.status}`)}
+					</Badge>
+				</Group>
+			</Group>
+			<Divider my={10} />
+			<Stack mt={15}>
+				<Stack gap={2}>
+					<Group justify="space-between">
+						<Group gap={10}>
+							<Title order={4}>Resource:</Title>
+							<Text size="lg">
+								{allocation.resource.name} ({allocation.resource.type})
+							</Text>
+						</Group>
+						<Text c="dimmed" size="sm">
+							Last modified: {dayjs(allocation.updatedAt).format('DD.MM.YYYY')}
+						</Text>
+					</Group>
+					<Group>
+						<Title order={4}>Quantity:</Title>
+						<Text>{allocation.quantity}</Text>
+					</Group>
+					<Group>
+						<Title order={4}>Start date:</Title>
+						<Text>{allocation.startDate ? allocation.startDate : '-not defined-'}</Text>
+					</Group>
+					<Group>
+						<Title order={4}>End date:</Title>
+						<Text>{allocation.endDate ? allocation.endDate : '-not defined-'}</Text>
+					</Group>
+				</Stack>
+				<Divider />
+				<Flex direction="column">
+					<Box>
+						<Text fw="bold">Justification:</Text>
+						{allocation.justification}
+					</Box>
+					<Box mt={10}>
+						<Text fw="bold">Description:</Text>
+						{allocation.description ? allocation.description : '-not defined-'}
+					</Box>
+				</Flex>
+			</Stack>
+
+			<Stack mt={20}>
+				<Title order={2}>Users in allocation</Title>
+				<DataTable
+					records={allocation.allocationUsers}
+					textSelectionDisabled
+					columns={[
+						{
+							accessor: 'id',
+							title: 'ID',
+							width: 70
+						},
+						{
+							accessor: 'name',
+							title: 'Name'
+						},
+						{
+							accessor: 'email',
+							title: 'E-mail'
+						}
+					]}
+				/>
+			</Stack>
 		</Box>
 	);
 };
